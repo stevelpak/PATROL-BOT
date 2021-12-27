27ff03714e850266703a7d06d84038ca8bfdf0a1
stevelpak <stevelpak@gmail.com>
2021-12-27 10:57:23 +0500

    First

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,5 +6,11 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Botni ishga tushurish"),
             types.BotCommand("help", "Yordam"),
+            types.BotCommand("ro", "Faqat o'qish rejimi"),
+            types.BotCommand("unro", "O'qish rejimidan chiqarish"),
+            types.BotCommand("ban", "Foydalanuvchini guruhdan haydash"),
+            types.BotCommand("set_photo", "Guruh rasmini o'zgartirish"),
+            types.BotCommand("set_title", "Guruh nomini o'zgartirish"),
+            types.BotCommand("set_description", "Guruh tavsifini o'zgartirish"),
         ]
     )
